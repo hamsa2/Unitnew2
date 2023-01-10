2699e6737cd0b433494ea4e3bcb17d79218ff7e2
Nailiya <66hamsa@gmail.com>
2023-01-10 11:34:07 +0700

    init

--- src/test/java/CashBackUnit2Test.java
+++ src/test/java/CashBackUnit2Test.java
@@ -1,6 +1,6 @@
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class CashBackUnit2Test {
 
@@ -19,6 +19,16 @@ public class CashBackUnit2Test {
         CashBackUnit2 cashBackUnit2 = new CashBackUnit2();
         int amount = 1100;
         int expected = 900;
+        int actual = cashBackUnit2.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void checkCash3() {
+        CashBackUnit2 cashBackUnit2 = new CashBackUnit2();
+        int amount = 1000;
+        int expected = 0;
         int actual = cashBackUnit2.remain(amount);
 
         assertEquals(actual, expected);
